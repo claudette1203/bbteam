3372c5bd1f90700fb434ed13c0f9b7f6f2d7a859
claudette1203 <c.scortia@student.tudelft.nl>
2025-05-04 19:54:44 +0200

    trying smth

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import News from './pages/News';
+import Sports from './pages/Sports';
+import Coaches from './pages/Coaches';
+import Contact from './pages/Contact';
+import Players from './pages/Players';
+import CoachDetail from './components/CoachDetail';
+import './App.css';
+import Legal from './pages/Legal';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div className="app-container">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/sports" element={<Sports />} />
+            <Route path="/coaches" element={<Coaches />} />
+            <Route path="/coaches/:id" element={<CoachDetail />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/players" element={<Players />} />
+            <Route path="/legal" element={<Legal />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
